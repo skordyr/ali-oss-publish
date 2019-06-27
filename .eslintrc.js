07cb2cea6c2024d8b9f83252c9146988f19ae588
skordyr <xyan.sk@gmail.com>
2019-06-27 14:27:01 +0800

    Update dependencies and add type declaration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  env: {
+    node: true,
+    es6: true
+  },
+  rules: {
+    semi: [
+      'error',
+      'never'
+    ]
+  }
+}
